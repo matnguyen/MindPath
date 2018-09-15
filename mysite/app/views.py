de085c75e015699c4767e2a1ef117762139bf885
hwl9 <hwl9@sfu.ca>
2018-09-15 11:27:56 -0700

    restructured

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("App index")
+    # return HttpResponse('<h1>Hi</h1>')
+    return render(request, 'templates/index.html')
+
+
+# from django.http import HttpResponse
+# from django.shortcuts import render
+#
+# def my_view(request):
+#     #return HttpResponse('<h1>Hello LumoHacks!</h1>')
+#
