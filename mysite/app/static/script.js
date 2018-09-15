ef603eed12dbe697df3e60e8c0f43cf77ad6fda4
hwl9 <hwl9@sfu.ca>
2018-09-15 12:50:31 -0700

    function for generating other avatars

--- mysite/app/static/script.js
+++ mysite/app/static/script.js
@@ -1,1 +1,21 @@
-console.log("Hello world!")
+var NUM_PEOPLE = 10;
+
+// Generates a <div> DOM object that represents an avatar_other.
+function createAvatarOther() {
+    var obj = $("<div class='avatar-other'></div>");
+    $('#avatar-others').append(obj);
+}
+
+function getNumOthers() {
+    return NUM_PEOPLE;
+}
+
+$(function() {
+
+    // Create avatars for each other person
+    var numOthers = getNumOthers();
+    for (var i = 0; i < numOthers; i++) {
+        createAvatarOther();
+    }
+
+});
