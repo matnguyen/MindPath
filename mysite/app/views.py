d2bd81e695a3b9e495ba2390832a09afb33e4e61
Matthew Nguyen <matthewnguyen.667@gmail.com>
2018-09-15 22:33:41 -0700

    Login/logout implemented

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,23 +1,44 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def authenticate_user(request):
+    username = request.POST.get("username", None)
+    password = request.POST.get("password", None)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
 
 def search(request):
     query = request.POST.get("handle", None)
     query = query.replace(" ", "+")
     return redirect("https://www.google.com/search?q=%s" % query)
 
+
 def apps(request):
     return render(request, 'apps.html')
+
 
 def clinics(request):
     return render(request, 'clinics.html')
 
+
 def call_centres(request):
     return render(request, 'call-centres.html')
+
 
 def about(request):
     return render(request, 'about.html')
