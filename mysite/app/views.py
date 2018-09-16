676bd58a453811a6cd7e3aab7a6cb32f2b291120
Matthew Nguyen <matthewnguyen.667@gmail.com>
2018-09-15 23:48:18 -0700

    Added user registration

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -17,6 +18,24 @@ def authenticate_user(request):
         login(request, user)
     if not user:
         messages.add_message(request, messages.ERROR, "Incorrect username or password.")
+    return redirect('/')
+
+
+def register_user(request):
+    username = request.POST.get("username", None)
+    email = request.POST.get("email", None)
+    password = request.POST.get("password", None)
+    password_confirm = request.POST.get("password_confirm", None)
+    if User.objects.filter(username=username).exists():
+        messages.add_message(request, messages.ERROR, "This username already exists.")
+    elif User.objects.filter(email=email).exists():
+        messages.add_message(request, messages.ERROR, "This email is already registered.")
+    elif password != password_confirm:
+        messages.add_message(request, messages.ERROR, "Your passwords do not match.")
+    else:
+        messages.add_message(request, messages.INFO, "Succesfully registered!")
+        user = User.objects.create_user(username, email, password)
+        user.save()
     return redirect('/')
 
 
