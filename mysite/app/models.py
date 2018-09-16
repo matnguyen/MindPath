db4264c92dc86ef714ba93a4247e04ccb30059c9
Matthew Nguyen <matthewnguyen.667@gmail.com>
2018-09-16 01:27:17 -0700

    Initial resources model

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Resources(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
     phone_no = models.CharField(max_length=20)
     health_type = models.CharField(max_length=50)
     resource_type = models.CharField(max_length=50)
-    link = models.CharField(max_length=500)
+    link = models.CharField(max_length=300)
